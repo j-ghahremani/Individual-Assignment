5ae0cc01c6c1bdb2a0c6eda53974ce0e9944b14b
Joel Ghahremani <joel.ghahremani@gmail.com>
2019-01-17 15:55:19 +0100

    Individual Assignment Python File

--- IA_python.py
+++ IA_python.py
@@ -1,5 +1,6 @@
 import json
 
+#Change the Pakistan.json file to exclude lines where high and low casualty estimates are equal to each other
 pakistan_fix = []
 with open('Pakistan.json', 'r') as file:
     data = json.load(file)
@@ -7,4 +8,14 @@ with open('Pakistan.json', 'r') as file:
         if line['high'] != line['low']:
             pakistan_fix.append(line)
 
-print(pakistan_fix)
+#For each line pakistan_fix, calculate the ratio between high and low casualty estimates
+#Then append date and casualty ratio pairs to a list for each pair
+#Append these pair lists to to a "list of lists" called final_list
+final_list = []
+for line in pakistan_fix:
+    y = []
+    y.append(line['date_end'])
+    x = line ['high'] / line ['low']
+    y.append(x)
+    final_list.append(y)
+
